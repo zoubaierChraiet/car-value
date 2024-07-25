971f069bb140dc487660bdfe4d04dea0945e2401
zoubaier chraiet <zouba.chraiet@yahoo.com>
2024-07-26 00:53:29 +0200

    init app

---
+++ src/users/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+} 
